832f289e0bfdbe3bf63b2a5a47cde3467d1a770f
jonathan de Montalembert <demonj92@gmail.com>
2012-07-20 15:44:19 +0800

    finished half

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-/* Author:
-
+/* Author: Jonathan de Montalembert
+Date: 2012.07.20
 */
+
+$("#dialogBox #whitePart textarea").focus(function(){
+	$(this).css({"color":"black"});
+}).blur(function(){
+	if($(this).val() <= 0)
+		$(this).css({"color":"#999999"});
+});
